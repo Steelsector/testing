ac0d739bacc771b3436b7f17cbc12e7c10075376
Viktor Bernáth <viktor0521@gmail.com>
2012-10-25 15:23:13 +0200

    oval drawing

---
+++ DrawOval/steelsec.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+public class steelsec {
+
+	public static void main(String[] args){
+		
+		//set window
+		TheWindow w = new TheWindow();
+		w.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		w.setSize(230,280);
+		w.setVisible(true);
+		
+		
+	}
+	
+	
+	
+}
